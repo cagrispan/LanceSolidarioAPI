94e4f600465eb7e23f4731d11b159d45e9d0a221
Carlos Augusto Grispan <cagrispan@gmail.com>
2016-09-16 14:43:43 -0300

    refactor(routes)

--- src/config/routes.config.js
+++ src/config/routes.config.js
@@ -1,16 +1,32 @@
-var UsersController = require('./../controllers/users/users.controller');
-var UsersMiddleware = require('./../middlewares/users.middleware');
 var AuthController = require('./../controllers/auth/auth.controller');
 var AuthMiddleware = require('./../middlewares/auth.middleware');
-var ProductController = require('./../controllers/product/product.controller');
-var ProductMiddleware = require('./../middlewares/product.middleware');
-
-var usersController = new UsersController();
-var usersMiddleware = new UsersMiddleware();
 var authController = new AuthController();
 var authMiddleware = new AuthMiddleware();
-var productController = new ProductController();
-var productMiddleware = new ProductMiddleware();
+
+var UsersController = require('./../controllers/users/users.controller');
+var UsersMiddleware = require('./../middlewares/users.middleware');
+var usersController = new UsersController();
+var usersMiddleware = new UsersMiddleware();
+
+var AddressesController = require('./../controllers/users/addresses.controller');
+var AddressesMiddleware = require('./../middlewares/addresses.middleware');
+var addressesController = new AddressesController();
+var addressesMiddleware = new AddressesMiddleware();
+
+var EmailsController = require('./../controllers/users/emails.controller');
+var EmailsMiddleware = require('./../middlewares/emails.middleware');
+var emailsController = new EmailsController();
+var emailsMiddleware = new EmailsMiddleware();
+
+var TelephonesController = require('./../controllers/users/telephones.controller');
+var TelephonesMiddleware = require('./../middlewares/telephones.middleware');
+var telephonesController = new TelephonesController();
+var telephonesMiddleware = new TelephonesMiddleware();
+
+var ProductsController = require('./../controllers/products/products.controller');
+var ProductsMiddleware = require('./../middlewares/products.middleware');
+var productsController = new ProductsController();
+var productsMiddleware = new ProductsMiddleware();
 
 module.exports = function (server) {
 
@@ -23,11 +39,70 @@ module.exports = function (server) {
         res.send(200);
     });
 
-    server.put('/users/:id', [authMiddleware.isLogged, usersMiddleware.hasAllInformation, usersController.addOrUpdate]);
-    server.get('/users/:id', [authMiddleware.isLogged, usersMiddleware.hasId, usersController.getSpecific]);
+    /*
+    Auth
+     */
+    server.post('/auth', authController.login);
 
-    server.post('/auth/:facebookId', authController.login);
+    /*
+    User
+     */
+    server.get('/users/:facebookId', [authMiddleware.isLogged, usersController.get]);
+    server.put('/users/:facebookId', [authMiddleware.isLogged, usersMiddleware.hasAllInformation, usersController.update]);
 
-    server.post('/products', [productMiddleware.hasAllInformation, productController.create]);
+    /*
+    Address
+     */
+    server.get('/users/:facebookId/addresses', [authMiddleware.isLogged, addressesController.getAll]);
+    server.post('/users/:facebookId/addresses', [authMiddleware.isLogged, addressesMiddleware.hasAllInformation, addressesController.add]);
+    server.put('/users/:facebookId/addresses/:addressId', [authMiddleware.isLogged, addressesMiddleware.hasId, addressesMiddleware.hasAllInformation, addressesController.update]);
+    server.del('/users/:facebookId/addresses/:addressId', [authMiddleware.isLogged, addressesMiddleware.hasId, addressesController.remove]);
 
+    /*
+    Email
+     */
+    server.get('/users/:facebookId/emails', [authMiddleware.isLogged, emailsController.getAll]);
+    server.post('/users/:facebookId/emails', [authMiddleware.isLogged, emailsMiddleware.hasAllInformation, emailsController.add]);
+    server.put('/users/:facebookId/emails/:emailId', [authMiddleware.isLogged, emailsMiddleware.hasId, emailsMiddleware.hasAllInformation, emailsController.update]);
+    server.del('/users/:facebookId/emails/:emailId', [authMiddleware.isLogged, emailsMiddleware.hasId, emailsController.remove]);
+
+    /*
+    Telephone
+     */
+    server.get('/users/:facebookId/telephones', [authMiddleware.isLogged, telephonesController.getAll]);
+    server.post('/users/:facebookId/telephones', [authMiddleware.isLogged, telephonesMiddleware.hasAllInformation, telephonesController.add]);
+    server.put('/users/:facebookId/telephones/:telephoneId', [authMiddleware.isLogged, telephonesMiddleware.hasId, telephonesMiddleware.hasAllInformation, telephonesController.update]);
+    server.del('/users/:facebookId/telephones/:telephoneId', [authMiddleware.isLogged, telephonesMiddleware.hasId, telephonesController.remove]);
+
+    /*
+    Products
+     */
+    server.get('/users/:facebookId/products', [authMiddleware.isLogged, productsController.getAll]);
+    server.post('/users/:facebookId/products', [authMiddleware.isLogged, productsMiddleware.hasAllInformation, productsController.add]);
+    server.put('/users/:facebookId/products/:productId', [authMiddleware.isLogged, productsMiddleware.hasId, productsMiddleware.hasAllInformation, productsController.update]);
+
+    //### User Auctions
+    //server.get('/users/:facebookId/auctions') //return auctions array
+    //server.post('/users/:facebookId/auctions') //create auction
+    //server.put('/users/:facebookId/auctions/:id') //update auction
+
+    //### User Bid
+    //server.get('/users/:facebookId/bids') //return bid array
+    //server.post('/users/:facebookId/bids') //create bid
+    //server.put('/users/:facebookId/bids/:id') //update bid
+
+    //### User Purchases
+    //server.get('/users/:facebookId/purchases') //return purchases array
+    //server.post('/users/:facebookId/purchases') //create purchase
+    //server.put('/users/:facebookId/purchases/:id') //update purchase
+
+    //### Auction
+    //server.get('/auctions') //return auctions array
+    //server.get('/auctions/:id') //return auction
+
+    //### Auction Bids
+    //server.get('/auctions/:id/bids') //return bids array
+
+    //### Auction Product
+    //server.get('/auctions/:id/products') //return products array
 };
