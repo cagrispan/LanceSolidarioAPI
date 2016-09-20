66a094fcf4748bb6dcca54d28e4039ffa14d8789
Bruno Henrique Pereira Braga <brunofaczz@hotmail.com>
2016-09-20 16:42:29 -0300

    test(UsersFacade.tests): create tests for Users Facade

--- tests/models/facades/Users/UsersFacade.tests.js
+++ tests/models/facades/Users/UsersFacade.tests.js
@@ -1,87 +1,83 @@
 'use strict';
 
 var expect = require('chai').expect;
-var UsersFacade = require('./../../../../src/models/facades/Users/UsersFacade');
+var usersFacade = require('./../../../../src/models/facades/UsersFacade');
 var q = require('q');
+var facebookIdToSearh;
+var makeId = require('./../../../makeId');
 
 describe('UsersFacade Tests', function () {
 
+    it('findOrCreate() should be a function', function (done) {
 
-    it('should be a function', function (done) {
-        var usersFacade = new UsersFacade();
-
-        expect(typeof usersFacade.createOrUpdate).to.equal('function');
+        expect(typeof usersFacade.findOrCreate).to.equal('function');
         done();
     });
 
-    it('should create or update an user on database', function (done) {
-        var usersFacade = new UsersFacade();
+    it('findOrCreate() should create an user on database', function (done) {
 
-        usersFacade.facebookId = 'teste123456';
-        usersFacade.email = 'teste@teste';
-        usersFacade.address = {
-            number: "123",
-            complement: "casa 1",
-            street: "rua",
-            city: "cidadezinha",
-            neighborhood: "vila",
-            state: "paranazao"
+        let user = {
+            facebookToken: 'facebookToken'+makeId(),
+            facebookId: 'facebookId'+makeId(),
+            birthday: new Date(),
+            name: 'name',
+            token: 'token'+makeId()
         };
-        usersFacade.birthday = new Date('10/09/1998');
-        usersFacade.name = 'testeZao';
-        usersFacade.facebookToken = 'tokenTest';
-        usersFacade.token = 'hahaha';
 
-        usersFacade.createOrUpdate().then(function (result) {
-            expect(result.dataValues.name).to.equal('testeZao');
+        facebookIdToSearh = user.facebookId;
+
+        usersFacade.findOrCreate(user).then((result) => {
+            expect(result[0].dataValues.name).to.equal('name');
             done();
-        }).catch(function (err) {
+        }).catch((err) => {
             console.log(err);
         });
 
     });
 
-    it('should update facebookToken if user already been created', function (done) {
-        var usersFacade = new UsersFacade();
+    it('read() should be a function', function (done) {
 
-        usersFacade.facebookId = 'teste123456';
-        usersFacade.email = 'teste@teste';
-        usersFacade.birthday = new Date('10/09/1998');
-        usersFacade.name = 'testeZao';
-        usersFacade.facebookToken = 'tokenTestLogin';
-        usersFacade.token = 'lalala';
+        expect(typeof usersFacade.read).to.equal('function');
+        done();
+    });
 
-        usersFacade.facebookLogin().then(function (result) {
-            expect(result.dataValues.facebookToken).to.equal('tokenTestLogin');
-            expect(result.dataValues.token).to.equal('hahaha');
+    it('read() should return an user on database', function (done) {
+
+        usersFacade.read(facebookIdToSearh).then((result) => {
+            expect(result.dataValues.name).to.equal('name');
             done();
+        }).catch((err) => {
+            console.log(err);
         });
 
     });
 
-    it('should verify that does not have an user and create it', function (done) {
-        var usersFacade = new UsersFacade();
+    it('update() should be a function', function (done) {
 
-        usersFacade.facebookId = makeid();
-        usersFacade.birthday = new Date('10/09/1998');
-        usersFacade.name = 'testeZao';
-        usersFacade.facebookToken = 'tokenTestLogin';
+        expect(typeof usersFacade.update).to.equal('function');
+        done();
+    });
 
-        usersFacade.facebookLogin().then(function (result) {
-            expect(result.dataValues.facebookToken).to.equal('tokenTestLogin');
+    it('update() should create an user on database', function (done) {
+
+        let user = {
+            facebookToken: 'facebookToken'+makeId(),
+            facebookId: facebookIdToSearh,
+            birthday: new Date(),
+            name: 'name1',
+            token: 'token'+makeId()
+        };
+
+
+        usersFacade.update(user).then((result) => {
+            console.log(result);
+            expect(result[0]).to.equal(1);
             done();
+        }).catch((err) => {
+            console.log(err);
         });
 
-        function makeid()
-        {
-            var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-            for( var i=0; i < 5; i++ )
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-            return text;
-        }
-
     });
+
+
 });
