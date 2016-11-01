7f4f459936bedf5098d92783c1928df5dd790afb
Bruno <bruno.braga@tuntscorp.com>
2016-11-01 17:45:54 -0200

    feat(auction): create middleware to valid auction

--- src/middlewares/auctions.middleware.js
+++ src/middlewares/auctions.middleware.js
@@ -1,4 +1,6 @@
 'use strict';
+var AuctionFacade = require('../models/facades/AuctionsFacade');
+
 function AuctionsMiddleware() {
 
     this.hasAllInformation = function (req, res, next) {
@@ -12,12 +14,43 @@ function AuctionsMiddleware() {
                 req.body.endDate) {
                 next();
             } else {
-                res.send(404, {message: 'Parameters missing.', receivedObject: req.body });
+                res.send(404, {message: 'Parameters missing.', receivedObject: req.body});
             }
         } else {
             res.send(404, {message: 'Parameters missing.'});
         }
 
+    };
+
+    this.hasOpenedAuction = function (req, res, next) {
+        let isValid = true;
+
+        AuctionFacade.readAllByProduct(req.params.facebookId, req.body.productId)
+            .then((data) => {
+                for (var i in data) {
+                    let startTime = new Date(data[i].dataValues.startDate);
+                    let endTime = new Date(data[i].dataValues.endDate);
+                    let currentDate = new Date();
+                    console.log(data[i].dataValues)
+                    if (currentDate > startTime && currentDate < endTime) {
+                        if (!data[i].isClosed && !data[i].isCanceled)
+                            isValid = false;
+                    }
+                }
+                if(isValid) {
+                    next();
+                } else {
+                    res.send(401, {message: 'This product has an auction pending'});
+                }
+            });
+    };
+
+    this.hasEnoughMinimumBid = function(req, res, next) {
+        if(req.params.minimumBid > 5) {
+            next();
+        } else {
+            res.send(401, {message: 'Minimum bid is less than 5 Temers.'});
+        }
     };
 
     this.hasId = function (req, res, next) {
